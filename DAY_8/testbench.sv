b5a5a862df99cfcc0fe01b100e1727409bc2978a
Nawras Ahamed <50738659+Nawras-Ahamed@users.noreply.github.com>
2023-04-29 11:01:26 +0530

    Update testbench.sv

--- DAY_8/testbench.sv
+++ DAY_8/testbench.sv
@@ -31,11 +31,9 @@ module BCD_ADDER_TB;
         a = 4;  b = 8;  c_in = 1;   #100;
         a = 4;  b = 1;  c_in = 0;   #100;
         a = 6;  b = 9;  c_in = 1;   #100;
-        a = 6;  b = 9;  c_in = 0;   #100;
+        a = 6;  b = 9;  c_in = 0;   #100; 
         a = 8;  b = 2;  c_in = 0;   #100;
         a = 8;  b = 2;  c_in = 1;   #100;
         a = 9;  b = 9;  c_in = 1;   #100;
-   
-    end
-     
+    end 
 endmodule
