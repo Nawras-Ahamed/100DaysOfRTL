30b4daaa44f2b830f06cc0a9b42944f82bfe571e
Nawras Ahamed <50738659+Nawras-Ahamed@users.noreply.github.com>
2023-05-06 22:54:26 +0530

    Add files via upload

--- DAY_11/testbench.sv
+++ DAY_11/testbench.sv
@@ -1,19 +1,30 @@
-`timescale 10ns/ps
+`timescale 10ns/1ps
 
 module encoder_tb;
+  
   reg [7:0] d;
+  wire [2:0] q;
+  
   encoder_8x3 DUT (.din(d), .dout(q));
+  
+  initial 
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+    end
+  
+  
 initial 
   begin
   
-    #10 d=8'b10000000;
-    #10 d=8'b01000000;
-    #10 d=8'b00100000;
-    #10 d=8'b00010000;
-    #10 d=8'b00001000;
-    #10 d=8'b00000100;
-    #10 d=8'b00000010;
-    #10 d=8'b00000001;
+    #10 d =8'b10000000;
+    #10 d =8'b01000000;
+    #10 d =8'b00100000;
+    #10 d =8'b00010000;
+    #10 d =8'b00001000;
+    #10 d =8'b00000100;
+    #10 d =8'b00000010;
+    #10 d =8'b00000001;
     
     #10 $finish;
 end
