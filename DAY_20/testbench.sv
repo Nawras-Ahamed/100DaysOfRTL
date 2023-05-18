1b5a3ca65e9a3c06a043c79d88bcbe6ba8bf1c36
Nawras Ahamed <50738659+Nawras-Ahamed@users.noreply.github.com>
2023-05-18 20:21:11 +0530

    Create testbench.sv

---
+++ DAY_20/testbench.sv
@@ -0,0 +1,36 @@
+`timescale 10ns/1ps
+
+module comparator_tb;
+
+    
+    reg [3:0] Din_A;
+    reg [3:0] Din_B;
+
+    
+    wire less;
+    wire equal;
+    wire greater;
+
+    
+    comparator DUT (
+        .Din_A(Din_A), 
+        .Din_B(Din_B), 
+        .less(less), 
+        .equal(equal), 
+        .greater(greater)
+    );
+
+    initial begin
+       
+        Din_A = 10;
+        Din_B = 12;
+        #100;
+        Din_A = 15;
+        Din_B = 11;
+        #100;
+        Din_A = 10;
+        Din_B = 10;
+        #100;
+    end
+      
+endmodule
