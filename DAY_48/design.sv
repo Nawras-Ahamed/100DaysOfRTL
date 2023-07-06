7da9b81fe5a5ee60de05c35475f1cdb951925dae
Nawras Ahamed <50738659+Nawras-Ahamed@users.noreply.github.com>
2023-07-06 21:07:01 +0530

    Add files via upload

---
+++ DAY_48/design.sv
@@ -0,0 +1,56 @@
+module seq_overlap(input clk, reset, data_stream,
+                          output mealy_output);
+  
+  
+localparam A=1,B=2,C=3,D=4; 
+  
+reg [3:0] present_state, next_state;
+  
+  
+  //dff block
+  
+  always @(posedge clk) 
+    begin
+    if(!reset) 
+      present_state <= A;
+    else 
+      present_state <= next_state;
+    end
+  
+ always @(present_state or data_stream) 
+    begin
+      case(present_state)
+        A: begin
+             if(data_stream == 0) 
+               next_state = A;
+             else       
+               next_state = B;
+           end
+        
+        B: begin
+             if(data_stream == 0) 
+               next_state = C;
+             else       
+               next_state = B;
+           end
+        
+        C: begin
+             if(data_stream == 0) 
+               next_state = A;
+             else       
+               next_state = D;
+           end
+      D: begin
+           if(data_stream == 0)
+             next_state = C;
+           else      
+             next_state = B;
+         end
+        
+      default: next_state = A;
+    endcase
+  end
+  
+  assign mealy_output = (present_state == D)? 1:0;
+  
+endmodule
---
+++ DAY_48/testbench.sv
@@ -0,0 +1,38 @@
+module seq_overlap_tb;
+  
+  
+reg clk,reset,data_stream;
+wire mealy_output;
+  
+seq_overlap DUT(clk,reset,data_stream,mealy_output);
+  
+initial 
+  begin 
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+
+  always #5 clk= ~clk;
+  
+  initial
+    begin
+      clk=0;
+      reset=1;
+      data_stream=0;
+      #10;
+      #20 reset = 0;
+      #5 reset  = 1;
+      #10 data_stream = 0;
+      #10 data_stream = 1;
+      #10 data_stream = 0;
+      #10 data_stream = 1;
+      #10 data_stream = 0;
+      #10 data_stream = 1;
+      #10 data_stream = 0;
+      #10 data_stream = 1;
+      #10 data_stream = 1;
+      #10 data_stream = 1;
+      #10 data_stream = 0;
+      #50 $finish;
+  end
+endmodule
